d131bee1a04e5250a1efc0cc6abb2683078a9e9f
Aidan Houlihan <aidandhoulihan@gmail.com>
2015-07-27 14:37:52 +1200

    added tests and javadocs

--- games.battleships/src/main/java/battleShipGame/Section.java
+++ games.battleships/src/main/java/battleShipGame/Section.java
@@ -14,21 +14,44 @@ public class Section{
         this.sectionStatus = sectionStatus;
         this.nodeID = shipID;
     }
-    
+
+    /**
+     *
+     * @return next node in the current ship section
+     */
     public Section getNextShipSection(){
         return this.nextNodeSection;
     }
+
+    /**
+     *
+     * @param section set the next section of the current ship section
+     */
     public void setNextShipSection(Section section){
         this.nextNodeSection = section;
     }
-    
+
+    /**
+     *
+     * @param status set the status of the section
+     */
     public void setSectionStatus(int status){
         this.sectionStatus = status;
     }
+
+
+    /**
+     *
+     * @return section status
+     */
     public int getSectionStatus(){
         return this.sectionStatus;
     }
-    
+
+
+    /**
+     *@return nodeID
+     */
     public int getShipID(){
         return this.nodeID;
     
