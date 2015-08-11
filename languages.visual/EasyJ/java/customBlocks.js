b050fc7c611597d6d6481cf578a67b4481e0ed5a
Mitchell Osborne <cloud202@hotmail.com>
2015-08-11 22:15:34 +1200

    added new custom blocks
    if_cell_hit_aim_direction
    can_attack_cell
    get_next_valid_cell
    get_neighbor_valid_cell

--- languages.visual/EasyJ/java/customBlocks.js
+++ languages.visual/EasyJ/java/customBlocks.js
@@ -7,9 +7,40 @@ Blockly.Java['get_state'] = function(block) {
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['get_cell_at_pos'] = function(block) {
+Blockly.Java['get_cell_at_pos'] = function(block) {
   var dropdown_position = block.getFieldValue('POSITION');
   var value_input = Blockly.JavaScript.valueToCode(block, 'INPUT', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.Java['if_cell_hit_aim_direction'] = function(block) {
+  var value_cell = Blockly.JavaScript.valueToCode(block, 'CELL', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_direction = block.getFieldValue('direction');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  return code;
+};
+
+Blockly.Java['can_attack_cell'] = function(block) {
+  var value_cell = Blockly.JavaScript.valueToCode(block, 'CELL', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.Java['get_next_valid_cell'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.Java['get_neighbour_valid_cells'] = function(block) {
+  var value_cell = Blockly.JavaScript.valueToCode(block, 'CELL', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
