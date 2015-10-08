2adb7210c24aca610aa3f04d948b6236a6230d12
Aidan Houlihan <aidandhoulihan@gmail.com>
2015-10-08 13:04:09 +0800

    usability changes for blockly

--- games.battleships/src/main/java/nz/daved/starbattle/bots/blocklyBot.java
+++ games.battleships/src/main/java/nz/daved/starbattle/bots/blocklyBot.java
@@ -1,23 +1,20 @@
 package nz.daved.starbattle.bots;
 
 
-import java.util.LinkedList;
+import java.util.LinkedList;;
 import nz.daved.starbattle.StarBattleBot;
 import nz.daved.starbattle.game.BotGameBoard;
 import nz.daved.starbattle.game.Coordinate;
 
-// <xml xmlns="http://www.w3.org/1999/xhtml"><block type="function_next_move" deletable="false" editable="false" x="63" y="63"><statement name="BLOCK"><block type="controls_if"><value name="IF0"><block type="last_move_state"><field name="STATE">1</field></block></value></block></statement></block></xml>
+// <xml xmlns="http://www.w3.org/1999/xhtml"><block type="function_next_move" deletable="false" editable="false" x="138" y="88"><value name="RETURN"><block type="lists_getIndex"><mutation statement="false" at="true"></mutation><field name="MODE">GET</field><field name="WHERE">FROM_START</field><value name="VALUE"><block type="list_of_played_moves"></block></value><value name="AT"><block type="math_number"><field name="NUM">1</field></block></value></block></value></block></xml>
 
-public class CustomStarBattleBot extends StarBattleBot {
+public class blocklyBot extends StarBattleBot {
 
-    public CustomStarBattleBot(String id) { super(id); }
+public blocklyBot(String id) { super(id); }
 
     @Override
     public Coordinate nextMove(BotGameBoard botGameBoard) {
-        if (botGameBoard.getLastMoveState(1)) {
-            {}
-        }
 
-        return ;
+        return botGameBoard.findAndHitNeighbour(new Coordinate(0, 0));
     }
 }
