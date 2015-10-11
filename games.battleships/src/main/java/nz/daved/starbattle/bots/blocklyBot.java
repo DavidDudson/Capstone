3e018776f34afe749ce6d97b7b3a12f7182abbe0
Aidan Houlihan <aidandhoulihan@gmail.com>
2015-10-11 20:10:53 +1300

    added missing docs

--- games.battleships/src/main/java/nz/daved/starbattle/bots/blocklyBot.java
+++ games.battleships/src/main/java/nz/daved/starbattle/bots/blocklyBot.java
@@ -1,28 +1,27 @@
 package nz.daved.starbattle.bots;
 
 
-import java.util.LinkedList;
 import nz.daved.starbattle.StarBattleBot;
 import nz.daved.starbattle.game.BotGameBoard;
 import nz.daved.starbattle.game.Coordinate;
+import nz.daved.starbattle.Var;
 
 // <xml xmlns="http://www.w3.org/1999/xhtml"><block type="function_next_move" deletable="false" editable="false" x="63" y="63"><value name="RETURN"><block type="get_first_valid_coordinate"></block></value></block></xml>
+
+
+// <xml xmlns="http://www.w3.org/1999/xhtml"><block type="function_next_move" deletable="false" editable="false" x="63" y="63"><statement name="BLOCK"><block type="variables_set"><field name="VAR">item</field><value name="VALUE"><block type="get_all_cells_of_type"><field name="state">0</field></block></value><next><block type="variables_set"><field name="VAR">item</field><value name="VALUE"><block type="math_number"><field name="NUM">0</field></block></value></block></next></block></statement><value name="RETURN"><block type="get_first_valid_coordinate"></block></value></block></xml>
 
 public class blocklyBot extends StarBattleBot {
 
     public blocklyBot(String id) { super(id); }
 
-    protected Boolean dfdfg = false;
-    protected Boolean werwer = false;
-    protected Boolean fgh = false;
-    protected Object fgfh;
+    protected Var item = new Var(0);
 
 
     @Override
     public Coordinate nextMove(BotGameBoard botGameBoard) {
-        dfdfg = botGameBoard.lastMoveSunkBot();
-        werwer = botGameBoard.getLastMoveState(1);
-        fgfh = 0;
+        item.setObject(botGameBoard.getCoordinatesWithState(0));
+        item.setObject(botGameBoard.checkStateOfCoordinate(new Coordinate(0, 0),0));
 
         return botGameBoard.getFirstValidCoordinate();
     }
